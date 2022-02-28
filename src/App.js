abbd4074798282186c85fe1db0a498736c8d1353
Megan Martel <mmartel1330@gmail.com>
2022-02-28 14:13:16 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { Rating } from '@mui/material';
-import React from 'react';
-import './App.css';
-import Restaurants from './components/Restaurants';
-import BasicRating from './components/Ratings'
+import { Rating } from "@mui/material";
+import React from "react";
+import "./App.css";
+import Restaurants from "./components/Restaurants";
+import BasicRating from "./components/Ratings";
 
 function App() {
   return (
     <div>
-    <h1>Restaurants</h1>
-    <Restaurants/>
+      <Restaurants />
     </div>
   );
 }
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,5 +1,23 @@
 import React, { useEffect, useState } from "react";
 import BasicRating from "./Ratings";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default function GetRestaurants() {
   const [restaurants, setRestaurants] = useState([]);
@@ -13,22 +31,42 @@ export default function GetRestaurants() {
 
   return (
     <>
+      <CssBaseline />
       {!restaurants ? (
         <h2>Loading...</h2>
       ) : (
-        <div>
-            <ul>
-          {restaurants.map((restaurant) => {
-            return <li>
-                <h2>{restaurant.name}</h2>
-                <img src={restaurant.photoUrl} />
-                <BasicRating rating={restaurant.rating} 
-                id={restaurant.id} 
-                setRestaurants={setRestaurants}/>
-            </li>
-          })}
-          </ul>
-        </div>
+        <Container maxWidth="lg">
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {restaurants.map((restaurant) => {
+              return (
+                <Grid item xs={4}>
+                    <Card sx={{ maxWidth: 345 }}>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={restaurant.photoUrl}
+                        alt={`Photo of ${restaurant.name}`}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {restaurant.name}
+                        </Typography>
+                        <BasicRating
+                          rating={restaurant.rating}
+                          id={restaurant.id}
+                          setRestaurants={setRestaurants}
+                        />
+                      </CardContent>
+                    </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
       )}
     </>
   );
